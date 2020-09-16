e4bf9ecab0229578f5e87bfaddf4ad500bfdce6e
Damien Pretet <damien.pretet@me.com>
2020-09-16 13:39:35 +0200

    Chore: Modify testsuite script to run in Travis-CI

--- sim/run.sh
+++ sim/run.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-# -e: exit if one command fails
-# -u: treat unset variable as an error
-# -f: disable filename expansion upon seeing *, ?, ...
-# -o pipefail: causes a pipeline to fail if any command fails
-set -euf -o pipefail
+echo "Start BSTer test flow"
 
-svutRun -test ./bster_testbench.sv
+svutRun -test ./bster_testbench.sv | tee log
+ret=$?
+
+if [[ $ret != 0 ]]; then
+    echo "Execution testsuite failed"
+    exit 1
+fi
+
+ec=$(grep -c "ERROR:" log)
+
+if [[ $ec != 0 ]]; then
+    echo "Execution suffered $ec issues"
+    exit 1
+fi
+
+echo "BSTer test flow successfully terminated ^^"
