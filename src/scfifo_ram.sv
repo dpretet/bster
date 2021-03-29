87c5de7b49efdaae50f1cf436dfd3e8cd06cb402
Damien Pretet <damien.pretet@me.com>
2021-03-29 20:51:30 +0200

    Fix: correct FIFO's RAM description wrongly declared data array

--- src/scfifo_ram.sv
+++ src/scfifo_ram.sv
@@ -11,19 +11,20 @@ module scfifo_ram
         parameter ADDR_WIDTH = 8,
         parameter DATA_WIDTH = 8
     )(
-        input  wire                  aclk,
-        input  wire                  wr_en,
-        input  wire [ADDR_WIDTH-1:0] addr_in,
-        input  wire [DATA_WIDTH-1:0] data_in,
-        input  wire [ADDR_WIDTH-1:0] addr_out,
-        output reg  [DATA_WIDTH-1:0] data_out
+        input  wire                   aclk,
+        input  wire                   wr_en,
+        input  wire  [ADDR_WIDTH-1:0] addr_in,
+        input  wire  [DATA_WIDTH-1:0] data_in,
+        input  wire  [ADDR_WIDTH-1:0] addr_out,
+        output logic [DATA_WIDTH-1:0] data_out
     );
 
-    reg [DATA_WIDTH-1:0] ram [$clog2(ADDR_WIDTH)-1:0];
+    logic [DATA_WIDTH-1:0] ram [2**ADDR_WIDTH-1:0];
 
     always @ (posedge aclk) begin
-        if (wr_en)
+        if (wr_en) begin
             ram[addr_in] <= data_in;
+        end
     end
 
     always @ (posedge aclk) begin
